3cc729fad28d63fcc547fd6461fddc864a2656b4
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-10-04 23:51:04 +0100

    Added Some Base Files

---
+++ metrorp/schema/factions/sh_dweller.lua
@@ -0,0 +1,13 @@
+--------------------------------------------------------------------------------------------------------
+FACTION.name = "Metro Dweller"
+--------------------------------------------------------------------------------------------------------
+FACTION.desc = "Commonfolk of the Metro"
+--------------------------------------------------------------------------------------------------------
+FACTION.color = Color(0, 112, 60)
+--------------------------------------------------------------------------------------------------------
+FACTION.isDefault = true
+--------------------------------------------------------------------------------------------------------
+FACTION.models = {"models/dizcordum/citizens/playermodels/pm_male_01.mdl", "models/dizcordum/citizens/playermodels/pm_male_02.mdl", "models/dizcordum/citizens/playermodels/pm_male_03.mdl", "models/dizcordum/citizens/playermodels/pm_male_04.mdl", "models/dizcordum/citizens/playermodels/pm_male_04.mdl", "models/dizcordum/citizens/playermodels/pm_male_05.mdl", "models/dizcordum/citizens/playermodels/pm_male_05.mdl", "models/dizcordum/citizens/playermodels/pm_male_08.mdl", "models/dizcordum/citizens/playermodels/pm_male_07.mdl", "models/dizcordum/citizens/playermodels/pm_male_05.mdl", "models/dizcordum/citizens/playermodels/pm_male_08.mdl", "models/dizcordum/citizens/playermodels/pm_male_07.mdl",} -- Metro Last Light Citizens
+--------------------------------------------------------------------------------------------------------
+FACTION_DWELLER = FACTION.index
+--------------------------------------------------------------------------------------------------------
