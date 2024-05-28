8d41c050d0cc0210a53cbd3e85b8f2da0a2453a8
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-28 10:26:23 +0000

    Added Temporary Values to Docs - bleonheart

--- metrorp/modules/radio/module.lua
+++ metrorp/modules/radio/module.lua
@@ -1,4 +1,3 @@
 ﻿MODULE.name = "Radio"
 MODULE.author = "76561198312513285"
 MODULE.discord = "@liliaplayer"
-MODULE.desc = "Radio"                                                                                                                                                                                    if nut then while true do return end end
