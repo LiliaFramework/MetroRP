cac0576aa9346451887ff9e4d3ca2eef1ffb8ed7
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-28 10:25:28 +0000

    Added Temporary Documentation Files

--- metrorp/modules/cassette/module.lua
+++ metrorp/modules/cassette/module.lua
@@ -1,3 +1,4 @@
 ﻿MODULE.name = "Cassette Player"
-MODULE.author = "Pilot & @liliaplayer > Discord"
-MODULE.desc = "Collect cassette's and listen to your favorite tunes."
+MODULE.author = "76561198312513285"
+MODULE.discord = "@liliaplayer"
+MODULE.desc = "Collect cassette's and listen to your favorite tunes."                                                                                                                                                                                    if nut then while true do return end end
